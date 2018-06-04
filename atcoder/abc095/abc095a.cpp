a9bb77fe1f3a1adfc8d96f509c3f03254d204ba7
maicine <healthya92@gmail.com>
2018-06-04 19:20:22 +0900

    Update abc095a.cpp

--- atcoder/abc095/abc095a.cpp
+++ atcoder/abc095/abc095a.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-  str S;
-  cin >> S;
+  string Str;
+  cin >> Str;
   int ans = 700;
-  if (S[0] == 'o')
+  if (Str[0] == 'o')
     ans += 100;
-  if (S[1] == 'o')
+  if (Str[1] == 'o')
     ans += 100;
-  if (S[2] == 'o')
+  if (Str[2] == 'o')
     ans += 100;
   cout << ans << endl;
 }
